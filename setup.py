777a9dfc62c63d3f632a2ac9ad928909363a8013
Fede Raimondo <f.raimondo@fz-juelich.de>
2025-04-10 22:03:43 +0200

    Update project config

--- setup.py
+++ setup.py
@@ -21,65 +21,8 @@
 # develop commercial activities as mentioned in the GNU Affero General Public
 # License version 3 without disclosing the source code of your own applications.
 #
-
-import os
-from os import path as op
-
-import setuptools  # noqa; we are using a setuptools namespace
-from distutils.core import setup
-from setuptools import find_packages
-
-# get the version (don't import mne here, so dependencies are not needed)
-version = None
-with open(os.path.join('cleaner', '__init__.py'), 'r') as fid:
-    for line in (line.strip() for line in fid):
-        if line.startswith('__version__'):
-            version = line.split('=')[1].strip().strip('\'')
-            break
-if version is None:
-    raise RuntimeError('Could not determine version')
-
-
-descr = """NICE python project for MEG and EEG data analysis (Cleaner)."""
-
-DISTNAME = 'cleaner'
-DESCRIPTION = descr
-MAINTAINER = '@fraimondo'
-MAINTAINER_EMAIL = 'federaimondo@gmail.com'
-URL = 'http://github.com/fraimondo/eeg_cleaner'
-LICENSE = 'GNU GPLv3'
-DOWNLOAD_URL = 'http://github.com/fraimondo/eeg_cleaner'
-VERSION = version
+from setuptools import setup
 
 
 if __name__ == "__main__":
-    if os.path.exists('MANIFEST'):
-        os.remove('MANIFEST')
-
-    setup(name=DISTNAME,
-          maintainer=MAINTAINER,
-          include_package_data=True,
-          maintainer_email=MAINTAINER_EMAIL,
-          description=DESCRIPTION,
-          license=LICENSE,
-          url=URL,
-          version=VERSION,
-          download_url=DOWNLOAD_URL,
-          long_description=open('README.rst').read(),
-          zip_safe=False,  # the package can run out of an .egg file
-          classifiers=['Intended Audience :: Science/Research',
-                       'Intended Audience :: Developers',
-                       'License :: OSI Approved',
-                       'Programming Language :: Python',
-                       'Topic :: Software Development',
-                       'Topic :: Scientific/Engineering',
-                       'Operating System :: Microsoft :: Windows',
-                       'Operating System :: POSIX',
-                       'Operating System :: Unix',
-                       'Operating System :: MacOS'],
-          platforms='any',
-          packages=find_packages(
-              exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-          # XXX put stuff here that's not gonna be shipped
-          package_data={'cleaner': []},
-          scripts=[])
+    setup()
